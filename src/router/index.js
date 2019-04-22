2043a2399f34084beae89a42738c9e798eabc4d8
wangguangyao <2459689222@qq.com>
2019-04-22 16:01:15 +0800

    master分支文件（无路由守卫）

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+import Login from '@/components/Login'
+import Home from '@/components/Home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {path: '/login',component: Login},
+    {path: '/home',component: Home}
+      // path: '/',name: 'HelloWorld',component: HelloWorld}
+  ]
+})
