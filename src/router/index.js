53300799412e5e401545b23cb7b4acf99120043a
wangguangyao <2459689222@qq.com>
2019-04-22 16:09:11 +0800

    用户登录功能开发完毕

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,23 @@ import Home from '@/components/Home'
 
 Vue.use(Router)
 
-export default new Router({
+const router= new Router({
   routes: [
     {path: '/login',component: Login},
     {path: '/home',component: Home}
       // path: '/',name: 'HelloWorld',component: HelloWorld}
   ]
 })
+
+// 给router设置路由守卫，非登录用户禁止访问
+router.beforeEach((to, from, next) => {
+  // 获取token，并判断是否存在
+  var token = window.sessionStorage.getItem('token')
+  if(token===null&& to.path!=='/login'){
+    // 强制跳转页面
+    return next('/login')
+  }
+  next() // 路由继续执行
+})
+
+export default router
